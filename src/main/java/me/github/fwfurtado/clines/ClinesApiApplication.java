b6fd626dc1982d04c2c39c5a9e1d0f45da9d9315
Nando <fwfurtado@gmail.com>
2020-03-23 02:41:56 -0300

    Feature/setup spring project (#2)
    * feat: Add spring dependencies
    
    * feat: Add maven wrapper support
    
    * feat: Add entrypoint class, empty configuration file and db migrations directory
    
    * feat: Add contracts directory
    
    * feat: Add flexible db configuration

---
+++ src/main/java/me/github/fwfurtado/clines/ClinesApiApplication.java
@@ -0,0 +1,12 @@
+package me.github.fwfurtado.clines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@checkstyle:off
+@SpringBootApplication
+public class ClinesApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClinesApiApplication.class, args);
+    }
+}
